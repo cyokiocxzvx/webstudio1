4b28dd8c5fe8619735a04524549eb436c2349c59
cyokiocxzvx <166243320+cyokiocxzvx@users.noreply.github.com>
2024-10-15 17:22:13 +0200

    qweq

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,21 @@
 (() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-open]"),
+        closeModalBtn: document.querySelector("[data-modal-close]"),
+        modal: document.querySelector("[data-modal]"),
+        backdrop: document.querySelector(".backdrop"),
+    };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+    refs.backdrop.addEventListener("click", (e) => {
+        if (e.target === refs.backdrop) {
+            toggleModal();
+        }
+    });
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-    document.body.classList.toggle("no-scroll");
-  }
+    function toggleModal() {
+        refs.backdrop.classList.toggle("is-hidden");
+        document.body.classList.toggle("no-scroll");
+    }
 })();
